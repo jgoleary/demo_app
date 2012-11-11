4ade5a62a56ec16a05db58cc53b605ffcb047f3e
John O'Leary <joleary@castlighthealth.com>
2012-11-11 14:46:43 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '8dcdb59aa7474c3fa3f951cc9f13af196a766362d23ee732a50c29520b06bf0ffee436ce9ff7d97123b325d095b2e68ea7655ae83598063653813ae0dc1852af'
